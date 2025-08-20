4129d1f8f990bbce86dc690167b188ad7fcb2392
souheil <119700614+souheil24@users.noreply.github.com>
2025-08-20 18:21:06 +0100

    Update urls.py

--- projectalger/urls.py
+++ projectalger/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from zonereghaia import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
